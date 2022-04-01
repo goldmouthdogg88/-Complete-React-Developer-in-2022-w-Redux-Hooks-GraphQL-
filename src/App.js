901bdb89010e8be7d577763abfa432e2855eb15a
goldmouthdogg88 <goldmouthdogg88@gmail.com>
2022-04-01 17:47:15 -0400

    Did some stuff

--- src/App.js
+++ src/App.js
@@ -8,25 +8,26 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: "Linda",
-          id: 0,
-        },
-        {
-          name: "Frank",
-          id: 1,
-        },
-        {
-          name: "Jackie",
-          id: 2,
-        },
-        {
-          name: "Andrei",
-          id: 3,
-        },
-      ],
+      monsters: [],
     };
+  }
+
+  // Code will run when component gets mounted
+  // A component gets mounted when React renders the element
+  // This process only happens once, throughout a component's life.
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
